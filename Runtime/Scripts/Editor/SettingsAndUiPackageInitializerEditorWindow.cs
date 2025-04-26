0930881d8d6b6a03ba9fb9a497bb8738044b889c
Oli Ollerenshaw <oli@mroshaw.co.uk>
2025-04-26 09:46:57 +0100

    Update for new core Editor UI framework

--- Runtime/Scripts/Editor/SettingsAndUiPackageInitializerEditorWindow.cs
+++ Runtime/Scripts/Editor/SettingsAndUiPackageInitializerEditorWindow.cs
@@ -6,20 +6,20 @@ namespace DaftAppleGames.Settings.Editor
 {
     public class SettingsAndUiPackageInitializerEditorWindow : PackageInitializerEditorWindow
     {
+        protected override string ToolTitle => "Settings and UI Installer";
+
+        protected override string WelcomeLogText =>
+            "Welcome to the Settings and UI Installer!";
+
+        protected override void CreateCustomGUI()
+        {
+        }
+
         [MenuItem("Daft Apple Games/Packages/Settings and UI")]
         public static void ShowWindow()
         {
             SettingsAndUiPackageInitializerEditorWindow packageInitWindow = GetWindow<SettingsAndUiPackageInitializerEditorWindow>();
             packageInitWindow.titleContent = new GUIContent("Settings and UI");
-        }
-        protected override string GetIntroText()
-        {
-            return "Welcome to Settings and UI!";
-        }
-
-        protected override string GetBaseInstallLocation()
-        {
-            return "Assets/SettingsAndUI";
         }
     }
 }
